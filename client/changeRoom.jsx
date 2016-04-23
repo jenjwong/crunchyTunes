c538bdd522765b195881028b8b548c9f7313f9ed
Kenneth Montgomery <kennethmontgo@gmail.com>
2016-04-23 12:34:44 -0700

    Fix styling

--- client/changeRoom.jsx
+++ client/changeRoom.jsx
@@ -4,29 +4,23 @@ import {Button, IconButton} from 'react-toolbox/lib/button';
 import Input from 'react-toolbox/lib/input';
 
 class ChangeRoom extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = {
       room:''
     };
   }
-
+  
   handleInputChange (room) {
     this.setState({
       room: room
     });
-
   }
-
-
-  //message from user to all other users
-
-
-
-
+  
   render() {
     return ( 
-      <div ref='chats' className='chats'>
+      <div>
         <Input type="text" style={{ width: '200px' }} label="" icon=""
         onChange={(value) =>this.handleInputChange(value)}  />
         <Button label='Change Room' raised onMouseUp={() => this.props.handleRoomChange(this.state.room)}/>
